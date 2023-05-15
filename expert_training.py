61bcbc74b240d25cc8daf072725f84fd7abe12f6
werkaaa <weronika.ormaniec@gmail.com>
2023-05-15 12:40:42 +0200

    Add trained half-cheetah.

--- expert_training.py
+++ expert_training.py
@@ -6,15 +6,15 @@ from stable_baselines3 import SAC
 env = gym.make("HalfCheetah-v4")
 env.reset(seed=69)
 
-# model = SAC("MlpPolicy", env, verbose=1)
-# model.learn(total_timesteps=100_000, log_interval=4, progress_bar=True)
-# model.save("cheetah")
+model = SAC("MlpPolicy", env, verbose=1)
+model.learn(total_timesteps=100_000, log_interval=4, progress_bar=True)
+model.save("cheetah")
 
 # # del model  # remove to demonstrate saving and loading
 
 env = gym.make("HalfCheetah-v4", render_mode="human")
 env.reset(seed=42)
-model = SAC.load("models/cheetah", env)
+model = SAC.load("experts/cheetah", env)
 
 NUM_SAMPLES = 10
 STEPS = 100
